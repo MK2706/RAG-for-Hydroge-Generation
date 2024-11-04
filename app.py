e9f451ade5040e7824b9561841880232f27428cf
MK2706 <mn2002manoj@gmail.com>
2024-11-04 16:09:41 +0530

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_ngrok import run_with_ngrok
 
-
+# Initialize the Flask application
 app = Flask(__name__)
+
+# Start Ngrok when the app is run
+run_with_ngrok(app)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Hello, World!"  # Response for the root URL
 
 if __name__ == '__main__':
-    app.run()
+    app.run()  # Run the application
